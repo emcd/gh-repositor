985b5a8ff28577245d44af6f674b366240d9f352
Eric McDonald <emcd@users.noreply.github.com>
2023-04-30 09:10:35 -0700

    Add Python virtual environment setup to automation. Move ChatGPT transcript to blog.

--- README.rst
+++ README.rst
@@ -3,6 +3,6 @@ For personal use.
 
 I know Python well, but was feeling lazy, so I coached OpenAI's GPT 4 language
 model on writing the Github repository creator. The `ChatGPT transcript
-<http://htmlpreview.github.io/?https://github.com/emcd/gh-repositor/blob/master/chat-gpt-github-repo-automation.html>`_
+<https://emcd.github.io/supplements/chat-gpt/github-repo-creator.html>`_
 shows that I spent more time providing feedback on the code than if I had
 just written it myself. But, it was fun.
--- create-repo.bash
+++ create-repo.bash
@@ -2,6 +2,11 @@
 
 set -eu -o pipefail
 
+if [[ -z "$1" ]]; then
+    echo 1>&2 'ERROR: Must supply project name.'
+    exit 1
+fi
+
 export GH_PROJECT_NAME="$1"
 
 export GITHUB_TOKEN="$(gh auth token)"
@@ -12,5 +17,12 @@ gha_gpg_signing_key_id="$(
     | tail -1 | awk '{print $1}')"
 export GPG_SIGNING_KEY="$(
     gpg --armor --export-secret-subkeys "${gha_gpg_signing_key_id}")"
+
+local -r venv_name='repo-creator-venv'
+rm --force --recursive "${venv_name}"
+python3 -m venv "${venv_name}"
+source "${venv_name}/bin/activate"
+python3 -m pip install --upgrade pip
+python3 -m pip install pynacl requests
 
 python3 create-repo.py
