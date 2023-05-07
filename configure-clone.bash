be3a069b38ec5ab3f77aec44f822ad1733e3bc79
Eric McDonald <emcd@users.noreply.github.com>
2023-05-07 11:27:48 -0700

    Fix username setup.

--- configure-clone.bash
+++ configure-clone.bash
@@ -8,6 +8,7 @@ my_gpg_signing_key_id="$(
     | tail -1 | awk '{print $2}' | awk -F/ '{print $2}')"
 
 git config user.email 'emcd@users.noreply.github.com'
+git config user.name 'Eric McDonald'
 git config user.signingkey "${my_gpg_signing_key_id}!"
 git config commit.gpgsign true
 git config tag.gpgsign true
